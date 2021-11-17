01e9fc630aa946c29eacfeeb3bd2d9e51a5cf2ca
Clivern <hello@clivern.com>
2021-11-17 15:06:44 +0100

    update

--- app/service/plan.py
+++ app/service/plan.py
@@ -12,7 +12,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from app.shortcuts import Logger
+from app.util.file_system import FileSystem
+
 
 class Plan():
     """Plan Service"""
-    pass
+
+    def __init__(self):
+        self.file_system = FileSystem()
+        self.logger = Logger().get_logger(__name__)
+
+    def get_plans(self):
+        """
+        Returns a list of enabled plans (community and custom ones)
+
+        Returns:
+            A list of enabled plans and their requirements
+        """
+        pass
+
+    def validate_plan_requirements(self, plan, inputs):
+        """
+        Validates a plan inputs
+
+        Args:
+            plan: The plan name
+            inputs: A dict of plan inputs
+
+        Returns:
+            Whether they are valid or not
+        """
+        pass
+
+    def create_plan_payload(self, plan, inputs):
+        """
+        Creates a plan payload from a valid inputs
+
+        Args:
+            plan: The plan name
+            inputs: A dict of plan inputs
+
+        Returns:
+            A dict of plan payload
+        """
+        pass
